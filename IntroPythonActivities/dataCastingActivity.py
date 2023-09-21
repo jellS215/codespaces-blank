0029cc13707ff9b097b45aa786511f4a112a6e64
jellS215 <144711104+jellS215@users.noreply.github.com>
2023-09-21 16:22:32 +0000

    class catchup

--- IntroPythonActivities/dataCastingActivity.py
+++ IntroPythonActivities/dataCastingActivity.py
@@ -1,7 +1,8 @@
 # convert the following datatypes into floats.
 12
 100
-
+float(12)
+float(100)
 # convert the following floats into integers.
 12.34
 23.33
@@ -15,7 +16,7 @@ num_two= 23.45
 print(num_one + num_two)
 
 # modify the following code below to 
-name_of_pet = "Spuny"
-age_of_pet = 10
+name_of_pet = "This is my dog Spuny, He is"
+age_of_pet = "10 years old"
 
-print("this is my dog "+ name_of_pet + ". He is " + age_of_pet+ " years old.")
+print(name_of_pet + age_of_pet)
