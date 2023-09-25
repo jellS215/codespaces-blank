609db32e53763e30e7a027c6b31e78059a2c0ff8
jellS215 <144711104+jellS215@users.noreply.github.com>
2023-09-25 16:11:01 +0000

    classwork 9/25

--- IntroPythonActivities/bultInFunctions.py
+++ IntroPythonActivities/bultInFunctions.py
@@ -6,3 +6,8 @@ pizza = 25/100
 quarters_of_pizza = pizza
 print(quarters_of_pizza)
 print(int(quarters_of_pizza))
+
+def welcome_Msg(UserName):
+    print('welcome to class' + str(UserName) + 'to class')
+
+    welcome_Msg()
---
+++ IntroPythonActivities/functionStarterActivity.py
@@ -0,0 +1,31 @@
+# 1. In your own words, describe what a function is
+"a function is a reusable block of code that only runs when its called"
+
+# 2. What is are function parameters and arguments and describe
+# the difference between the 2. 
+"function parameter is a place holder - goes in function definition"
+"function argument is the actual data - goes in function call"
+
+# 3. write a function that will print out a welcome message
+# that includes a users name. You will need to use parameters and arguments
+def welcome_msg(username):
+    print('welcome '+ username + ' to coding class')
+    
+    welcome_msg('nahjell')
+
+# 4. Write a function that will do a calculation that takes 3 parameters.
+# Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
+def calculate(randNumber, randomNumber2, randomNumber3):
+    print(randNumber + randomNumber2 + randomNumber3)
+    
+    calculate(64, 73, 200)
+    
+    
+# 5. Write a function that will output a message to a user, telling them
+# what class they have next after this one. this code should use a 
+# variable to pass a value into the parameter of a function. The variable should
+# be real, user data- not hard coded data.  
+
+def userMsg(nextClass):
+    className = input('what is your next class')
+    print("you have" + nextClass + "after this")
